e5146e360b284b20a2a04a8f6c0568d08b3e97bd
andbamp <andreasbampouris@gmail.com>
2022-05-06 17:53:24 +0300

    Fix test description

--- tests/testthat/test-isco_soc_crosswalk.R
+++ tests/testthat/test-isco_soc_crosswalk.R
@@ -30,7 +30,7 @@ test_that("ISCO_3 to SOC_4 crosswalk is OK!", {
   expect_true(soc_estimate[, all(!is.na(soc10))])
 })
 
-test_that("ISCO_3 to SOC_1 crosswalk for indicators is OK!", {
+test_that("ISCO_3 to SOC_3 crosswalk for indicators is OK!", {
   soc_estimate <- isco_soc_crosswalk(isco_3,
                                      soc_lvl = "soc_3", indicator = TRUE)
   expect_true(isco_3[, sum(value)] <= soc_estimate[, sum(value)])
